42c67b9ae71e4b8717b68e365ab90adee51f998b
nclavaud <nicolas@lrqdo.fr>
2015-07-27 22:28:17 +0200

    Move JSON data source in separate file

--- app.js
+++ app.js
@@ -12,35 +12,35 @@ var Page = React.createClass({
                     <li role="display" onClick={this.changeDisplay.bind(this, 'table')} className={'table' == this.state.display ? 'active' : ''}><a href="#">Table</a></li>
                     <li role="display" onClick={this.changeDisplay.bind(this, 'blocks')} className={'blocks' == this.state.display ? 'active' : ''}><a href="#">Blocks</a></li>
                 </ul>
-                <CoverList display={this.state.display} />
+                <CoverList url="resources.json" display={this.state.display} />
             </div>
         );
     }
 });
 
 var CoverList = React.createClass({
-    resources: function() {
-        return [
-            {
-                source: "bandcamp",
-                url: "https://strandflat.bandcamp.com/track/ten-years-from-the-second",
-                title: "Ten years from the second, by Have The Moskovik",
-                image: "https://f1.bcbits.com/img/a3801425696_5.jpg",
-                player: "https://bandcamp.com/EmbeddedPlayer/v=2/track=694908547/size=large/linkcol=0084B4/notracklist=true/twittercard=true/"
-            },
-            {
-                source: "soundcloud",
-                url: "https://soundcloud.com/havethemoskovik/what-a-wonderful-place-this-earth-would-be",
-                title: "What A Wonderful Place This Earth Would Be",
-                image: "https://i1.sndcdn.com/artworks-000080845250-7wvy52-t200x200.jpg",
-                player: "https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F151837891&amp;auto_play=false&amp;show_artwork=true&amp;visual=true&amp;origin=twitter",
-            }
-        ];
+    loadData: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({resources: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    componentDidMount: function() {
+        this.loadData();
+    },
+    getInitialState: function() {
+        return {resources: []};
     },
     render: function() {
         if ('blocks' == this.props.display) {
-            var covers = this.resources().map(function (cover) {
-                return <Cover source={cover.source} title={cover.title} image={cover.image} url={cover.url} />
+            var covers = this.state.resources.map(function (cover) {
+                return <Cover key={cover.uuid} source={cover.source} title={cover.title} image={cover.image} url={cover.url} />
             });
 
             return (
@@ -48,8 +48,8 @@ var CoverList = React.createClass({
             );
         }
 
-        var rows = this.resources().map(function (cover) {
-            return <CoverAsTableRow source={cover.source} title={cover.title} image={cover.image} url={cover.url} />
+        var rows = this.state.resources.map(function (cover) {
+            return <CoverAsTableRow key={cover.uuid} source={cover.source} title={cover.title} image={cover.image} url={cover.url} />
         });
 
         return (
