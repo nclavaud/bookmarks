045ec2b1a744b6df1cb4f172f0055be659f96d90
nclavaud <nicolas@lrqdo.fr>
2015-08-12 23:45:38 +0200

    [api] Create SQLite database

---
+++ api/resources/sql/fixtures.sql
@@ -0,0 +1,65 @@
+INSERT INTO bookmarks (uuid, url, data) VALUES
+    (
+        '00000000-0000-0000-0000-000000000bc1',
+        'https://strandflat.bandcamp.com/track/ten-years-from-the-second',
+        '{
+            "source": "bandcamp.com",
+            "type": "song",
+            "title": "Ten years from the second, by Have The Moskovik",
+            "image": "https://f1.bcbits.com/img/a3801425696_5.jpg",
+            "player": "https://bandcamp.com/EmbeddedPlayer/v=2/track=694908547/size=large/linkcol=0084B4/notracklist=true/twittercard=true/"
+        }'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000bc2',
+        'https://deltasleep.bandcamp.com/album/twin-galaxies-2',
+        '{
+            "source": "bandcamp.com",
+            "type": "album",
+            "title": "Twin Galaxies, by Delta Sleep",
+            "image": "https://f1.bcbits.com/img/a3750314634_5.jpg",
+            "player": "https://bandcamp.com/EmbeddedPlayer/v=2/album=3424703028/size=large/linkcol=0084B4/notracklist=true/twittercard=true/"
+        }'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000sc1',
+        'https://soundcloud.com/havethemoskovik/temporaire',
+        '{
+            "source": "soundcloud.com",
+            "type": "soundcloud:sound",
+            "title": "Temporaire",
+            "image": "https://i1.sndcdn.com/artworks-000079499252-4vip0n-t200x200.jpg",
+            "player": "https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F149629549&amp;auto_play=false&amp;show_artwork=true&amp;visual=true&amp;origin=twitter"
+        }'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000es1',
+        'http://www.esprit.fr/tunique-100-coton-075CC1F001_620',
+        '{
+            "source": "esprit.fr",
+            "type": "product",
+            "title": "Tunique 100 % coton",
+            "image": "http://pics3.esprit.de/Shop/PicPOV_MainView/075CC1F001_620.jpg"
+        }'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000ws1',
+        'https://facebook.github.io/react/index.html',
+        '{
+            "source": "github.io",
+            "type": "website",
+            "title": "A JavaScript library for building user interfaces | React",
+            "image": "https://facebook.github.io/react/img/logo_og.png"
+        }'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000af1',
+        'http://fr.audiofanzine.com/looper/tc-electronic/ditto-looper/',
+        '{
+            "source": "audiofanzine.com",
+            "type": "product",
+            "title": "TC Electronic Ditto Looper",
+            "image": "http://img.audiofanzine.com/image.php?lang=fr&identifier=id&size=normal&module=product&product_id=172615"
+        }'
+    )
+;
---
+++ api/resources/sql/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE bookmarks (
+    uuid VARCHAR(36) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    data TEXT
+);
