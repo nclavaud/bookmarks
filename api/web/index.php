2fab6d97ebcca45b58efe2aab18dc7ded8cb8bb9
nclavaud <nicolas@lrqdo.fr>
2015-08-12 23:46:51 +0200

    [api] Fetch resources from database

--- api/web/index.php
+++ api/web/index.php
@@ -2,12 +2,23 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
 $app['debug'] = getenv('DEBUG');
 
+$app['bookmark.repository'] = function () {
+    return new App\BookmarkRepository(new \PDO(getenv('DSN')));
+};
+
+$app->after(function (Request $request, Response $response) {
+    $response->headers->set('Access-Control-Allow-Origin', '*');
+});
+
 $app->get('/', function () use ($app) {
-    $resources = array();
+    $resources = $app['bookmark.repository']->findAll();
 
     return json_encode($resources);
 });
