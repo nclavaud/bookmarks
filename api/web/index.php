bc92f5ffd1dca1d3a2a3ce60dc76a537034cb9d7
nclavaud <nicolas@lrqdo.fr>
2015-08-23 15:35:48 +0200

    [api] Publish AMQP message after bookmark has been created
      * added RabbitMQ configuration
      * publish an async event message after bookmark has been created

--- api/web/index.php
+++ api/web/index.php
@@ -2,11 +2,33 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use fiunchinho\Silex\Provider\RabbitServiceProvider;
 use Rhumsaa\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
+
+$app->register(new RabbitServiceProvider(), array(
+    'rabbit.connections' => array(
+        'default' => array(
+            'host' => 'rabbitmq',
+            'port' => 5672,
+            'user' => 'guest',
+            'password' => 'guest',
+            'vhost' => '/',
+        ),
+    ),
+    'rabbit.producers' => array(
+        'bookmark' => array(
+            'connection' => 'default',
+            'exchange_options' => array(
+                'name' => 'bookmark_exchange',
+                'type' => 'topic',
+            ),
+        ),
+    ),
+));
 
 $app['debug'] = getenv('DEBUG');
 
@@ -28,6 +50,12 @@ $app->post('/', function (Request $request) use ($app) {
     $bookmark = new App\Bookmark(Uuid::uuid4(), new App\Url($request->request->get('url')));
 
     $app['bookmark.repository']->save($bookmark);
+
+    $message = json_encode((object) array(
+        'event' => 'bookmark_has_been_created',
+        'uuid' => (string) $bookmark->getUuid(),
+    ));
+    $app['rabbit.producer']['bookmark']->publish($message);
 
     return $app->json($bookmark, 201);
 });
